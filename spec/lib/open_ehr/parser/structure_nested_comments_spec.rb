ea29a79b1662c5c0c50de9348df7bc1d40139ce9
Shinji KOBAYASHI <skoba@moss.gr.jp>
2011-12-13 23:13:21 +0900

    fixed #187

--- spec/lib/open_ehr/parser/structure_nested_comments_spec.rb
+++ spec/lib/open_ehr/parser/structure_nested_comments_spec.rb
@@ -5,17 +5,18 @@ require File.dirname(__FILE__) + '/parser_spec_helper'
 describe ADLParser do
   context 'Structure spec' do
     before(:all) do
-      archetype = adl14_archetype('adl-test-entry.structure_test2.test.adl')
+      archetype = adl14_archetype('openEHR-EHR-CLUSTER.auscultation.v1.adl')
       @definition = archetype.definition
     end
 
-    it 'rm type name is ENTRY' do
-      @definition.rm_type_name.should == 'ENTRY'
+    it 'rm type name is CLUSTER' do
+      @definition.rm_type_name.should == 'CLUSTER'
     end
 
     it '1st attribute type name is items' do
       @definition.attributes[0].rm_attribute_name.should ==
-        'subject_relationship'
+        'items'
     end
   end
 end
+
