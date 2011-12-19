c7cdeb77762d876607f3bc3a49799fe18dd595a2
Shinji KOBAYASHI <skoba@moss.gr.jp>
2011-12-19 23:34:59 +0900

    refs #174

--- spec/lib/open_ehr/parser/c_dv_quantity_parse_spec.rb
+++ spec/lib/open_ehr/parser/c_dv_quantity_parse_spec.rb
@@ -1,10 +1,11 @@
+# ticket 174
 require File.dirname(__FILE__) + '/../../../spec_helper'
 require File.dirname(__FILE__) + '/parser_spec_helper'
 require 'open_ehr/am/openehr_profile/data_types/quantity'
 include OpenEHR::AM::OpenEHRProfile::DataTypes::Quantity
 
 describe ADLParser do
-  describe CDvOrdinal do
+  describe CDvQuantity do
     before(:all) do
       archetype = adl14_archetype('adl-test-entry.c_dv_quantity_full.test.adl')
       @attributes = archetype.definition.attributes
