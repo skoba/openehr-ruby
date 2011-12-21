16aaa98f8bafcc73eca71ed2bea3450648ba757e
Shinji KOBAYASHI <skoba@moss.gr.jp>
2011-12-21 22:55:13 +0900

    fixed #197

--- lib/open_ehr/am/openehr_profile/data_types/basic.rb
+++ lib/open_ehr/am/openehr_profile/data_types/basic.rb
@@ -32,6 +32,42 @@ module OpenEHR
           end
 
           class Transition
+            attr_reader :event, :action, :guard, :next_state
+
+            def initialize(args = { })
+              self.event = args[:event]
+              self.guard = args[:guard]
+              self.action = args[:action]
+              self.next_state = args[:next_state]
+            end
+
+            def event=(event)
+              if event.nil? or event.empty?
+                raise ArgumentError, 'event is mandatory'
+              end
+              @event = event
+            end
+
+            def guard=(guard)
+              if !guard.nil? && guard.empty?
+                raise ArgumentError, 'guard should not be empty'
+              end
+              @guard = guard
+            end
+
+            def action=(action)
+              if !action.nil? && action.empty?
+                raise ArgumentError, 'action should not be empty'
+              end
+              @action = action
+            end
+
+            def next_state=(next_state)
+              if next_state.nil?
+                raise ArgumentError, 'next state is mandatory'
+              end
+              @next_state = next_state
+            end
           end
         end
       end # of DataTypes
