f42ddd47b8787d841f42727a0927ec0a7967b877
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-10-23 21:22:14 +0900

    OptParser launched

--- lib/openehr/parser/opt_parser.rb
+++ lib/openehr/parser/opt_parser.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module OpenEHR
   module Parser
     class OPTParser < ::OpenEHR::Parser::Base
@@ -6,8 +8,10 @@ module OpenEHR
       end
 
       def parse
-        xml = Nokogiri::XML::Document.parse(File(__FILE__ + '/eReferral.opt'))
-        terminology = OpenEHR::RM::Support::Identification::TerminologyID.new(value:)
+        opt = Nokogiri::XML::Document.parse(File.open(@filename))
+        opt.remove_namespaces!
+        terminology_id = OpenEHR::RM::Support::Identification::TerminologyID.new(value: opt.xpath('/template/language/terminology_id/value').text)
+        language = OpenEHR::RM::DataTypes::Text::CodePhrase.new(code_string: opt.xpath('/template/language/code_string').text, terminology_id: terminology_id)
         OpenEHR::AM::Template::OperationalTemplate.new(language: language)
       end
     end
--- spec/lib/openehr/opt_parser/opt_parser_spec.rb
+++ spec/lib/openehr/opt_parser/opt_parser_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 module OpenEHR
   module Parser
     describe OPTParser do
-      let(:optparser) { OpenEHR::Parser::OPTParser.new('./eReferral.opt')}
+      let(:optparser) { OpenEHR::Parser::OPTParser.new(File.join(File.dirname(__FILE__), './eReferral.opt'))}
       let(:opt) {optparser.parse}
 
-      # it 'language code string is en' do
-      #   expect(opt.description.language.code_string).to eq 'en'
-      # end
+      it 'language code string is en' do
+        expect(opt.language.code_string).to eq 'en'
+      end
     end
   end
 end
