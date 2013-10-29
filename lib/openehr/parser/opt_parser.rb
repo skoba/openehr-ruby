ba5f9720ea0dcef39454e6b8ae3353dad93227b3
Shinji KOBAYASHI <skoba@moss.gr.jp>
2013-10-30 00:31:46 +0900

    refactored opt_parser description

--- lib/openehr/parser/opt_parser.rb
+++ lib/openehr/parser/opt_parser.rb
@@ -14,29 +14,37 @@ module OpenEHR
       DESC_DETAILS_USE_PATH = '/template/description/details/use'
       DESC_DETAILS_MISUSE_PATH = '/template/description/details/misuse'
       DESC_DETAILS_COPYRIGHT_PATH = '/template/description/details/copyright'
+
       def initialize(filename)
         super(filename)
       end
 
       def parse
-        opt = Nokogiri::XML::Document.parse(File.open(@filename))
-        terminology_id = OpenEHR::RM::Support::Identification::TerminologyID.new(value: text_on_path(opt,TEMPLATE_LANGUAGE_TERM_ID_PATH))
-        language = OpenEHR::RM::DataTypes::Text::CodePhrase.new(code_string: text_on_path(opt, TEMPLATE_LANGUAGE_CODE_PATH), terminology_id: terminology_id)
-        original_author = text_on_path(opt, DESC_ORIGINAL_AUTHOR_PATH)
-        lifecycle_state = text_on_path(opt, DESC_LIFECYCLE_STATE_PATH)
-        details_terminology_id = OpenEHR::RM::Support::Identification::TerminologyID.new(value: text_on_path(opt, DESC_DETAILS_LANGUAGE_TERM_ID_PATH))
-        details_language = OpenEHR::RM::DataTypes::Text::CodePhrase.new(code_string: text_on_path(opt, DESC_DETAILS_LANGUAGE_CODE_PATH), terminology_id: details_terminology_id)
-        details_purpose = text_on_path(opt, DESC_DETAILS_PURPOSE_PATH)
-        details_keywords = opt.xpath(DESC_DETAILS_KEYWORDS_PATH).inject([]) {|a, i| a << i.text}
-        details_use = empty_then_nil text_on_path(opt, DESC_DETAILS_USE_PATH)
-        details_misuse = empty_then_nil text_on_path(opt, DESC_DETAILS_MISUSE_PATH)
-        details_copyright = empty_then_nil text_on_path(opt, DESC_DETAILS_COPYRIGHT_PATH)
-        details = OpenEHR::RM::Common::Resource::ResourceDescriptionItem.new(language: details_language, purpose: details_purpose, keywords: details_keywords, use: details_use, misuse: details_misuse, copyright: details_copyright)
-        description = OpenEHR::RM::Common::Resource::ResourceDescription.new(original_author: original_author, lifecycle_state: lifecycle_state, details: [details])
+        @opt = Nokogiri::XML::Document.parse(File.open(@filename))
+        terminology_id = OpenEHR::RM::Support::Identification::TerminologyID.new(value: text_on_path(@opt,TEMPLATE_LANGUAGE_TERM_ID_PATH))
+        language = OpenEHR::RM::DataTypes::Text::CodePhrase.new(code_string: text_on_path(@opt, TEMPLATE_LANGUAGE_CODE_PATH), terminology_id: terminology_id)
         OpenEHR::AM::Template::OperationalTemplate.new(language: language, description: description)
       end
 
       private
+
+      def description
+        original_author = text_on_path(@opt, DESC_ORIGINAL_AUTHOR_PATH)
+        lifecycle_state = text_on_path(@opt, DESC_LIFECYCLE_STATE_PATH)
+        OpenEHR::RM::Common::Resource::ResourceDescription.new(original_author: original_author, lifecycle_state: lifecycle_state, details: [description_details])
+      end
+
+      def description_details
+        terminology_id = OpenEHR::RM::Support::Identification::TerminologyID.new(value: text_on_path(@opt, DESC_DETAILS_LANGUAGE_TERM_ID_PATH))
+        language = OpenEHR::RM::DataTypes::Text::CodePhrase.new(code_string: text_on_path(@opt, DESC_DETAILS_LANGUAGE_CODE_PATH), terminology_id: terminology_id)
+        purpose = text_on_path(@opt, DESC_DETAILS_PURPOSE_PATH)
+        keywords = @opt.xpath(DESC_DETAILS_KEYWORDS_PATH).inject([]) {|a, i| a << i.text}
+        use = empty_then_nil text_on_path(@opt, DESC_DETAILS_USE_PATH)
+        misuse = empty_then_nil text_on_path(@opt, DESC_DETAILS_MISUSE_PATH)
+        copyright = empty_then_nil text_on_path(@opt, DESC_DETAILS_COPYRIGHT_PATH)
+        OpenEHR::RM::Common::Resource::ResourceDescriptionItem.new(language: language, purpose: purpose, keywords: keywords, use: use, misuse: misuse, copyright: copyright)
+      end
+
       def empty_then_nil(val)
         if val.empty?
           return nil
